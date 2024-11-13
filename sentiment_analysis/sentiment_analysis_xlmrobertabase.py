03b5b8c56726e84e44d16d76e74cab2f2f835602
Stefania Sava <sava.stefania@yahoo.com>
2024-11-13 18:14:42 +0100

    Update the xlmrobertabase model as well

--- sentiment_analysis/sentiment_analysis_xlmrobertabase.py
+++ sentiment_analysis/sentiment_analysis_xlmrobertabase.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 # Load pre trained XLM-RoBERTa model for sentiment-analysis
-sentiment_pipeline = pipeline("sentiment-analysis", model="cardiffnlp/twitter-xlm-roberta-base-sentiment")
+roberta_sentiment_pipeline = pipeline("sentiment-analysis", model="cardiffnlp/twitter-xlm-roberta-base-sentiment")
 
 # Label mapping for the sentiment output
 label_mapping = {
@@ -20,29 +20,9 @@ def analyze_sentiment(text):
     Returns:
         dict: A dictionary with the raw output of the model.
     """
-    return sentiment_pipeline(text)[0]    
+    roberta_output = roberta_sentiment_pipeline(text)[0]
+    return {
+                "label": roberta_output["label"],
+                "score": roberta_output["score"]
+             }    
     
-
-
-def categorize_text(sentiment_result, positive_threshold=0.5, negative_threshold=0.5):
-    """
-    Categorize the sentiment as positive, negative, or neutral based on thresholds.
-
-    Args:
-        sentiment_result (dict): The result from the sentiment analysis containing label and score.
-        positive_threshold (float): The threshold above which the sentiment is considered positive.
-        negative_threshold (float): The threshold below which the sentiment is considered negative.
-
-    Returns:
-        str: The category of the sentiment ('positive', 'negative', 'neutral').
-    """
-    label = sentiment_result['label']
-    score = sentiment_result['score']
-    
-    if label == 'LABEL_2' and score >= positive_threshold:
-        return "positive"
-    elif label == 'LABEL_0' and score <= negative_threshold:
-        return "negative"
-    else:
-        return "neutral"
-
